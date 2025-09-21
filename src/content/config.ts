af3201c15da73a19a74c10ab9efbe3f15195008e
Philip Breuer <philip.breuer@pm.me>
2025-09-21 23:01:59 +0200

    Add hashcode page with download buttons

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,54 +1,55 @@
-import { defineCollection, z } from "astro:content";
-import { Document } from "mupdf";
+import {defineCollection, z} from "astro:content";
+import {Document} from "mupdf";
 
 export const collections = {
-	hashcodes: defineCollection({
-		type: "content_layer",
-		loader: async () => {
-			const pdfGlob = import.meta.glob("./hashcodes/**/*.pdf");
-			const inputsGlob = import.meta.glob([
-				"./hashcodes/**/*.in",
-				"./hashcodes/**/*.in/*.txt",
-			]);
+    hashcodes: defineCollection({
+        type: "content_layer",
+        loader: async () => {
+            const globToFiles = (glob) => Promise.all(
+                Object.entries(glob).map(async ([file, url]) => ({
+                    path: file,
+                    name: file.split("/").at(-1) || '',
+                    url: (await url()).default,
+                })),
+            )
 
-			return Object.keys(pdfGlob).map((file) => {
-				const [dot, type, year, round] = file.split("/");
-				const folder = [dot, type, year, round].join("/");
-				const inputs = Object.keys(inputsGlob).filter((file) =>
-					file.startsWith(folder),
-				);
+            const pdfGlob = import.meta.glob("./hashcodes/**/*.pdf", {query: "?url"});
+            const inputsGlob = import.meta.glob(
+                ["./hashcodes/**/*.in", "./hashcodes/**/*.in/*.txt"],
+                {query: "?url"},
+            );
+            const allPdfFiles = await globToFiles(pdfGlob);
+            const allInputFiles = await globToFiles(inputsGlob)
 
-				const pdf = Document.openDocument("src/content/" + file);
-				const page = JSON.parse(pdf.loadPage(0).toStructuredText().asJSON());
-				const lines = page.blocks
-					.flatMap((block) => block.lines)
-					.map((line) => line.text.trim());
-				const [title] = lines
-					.filter((text) => text.length > 3) // Filter things like "EN"
-					.filter((text) => !/\d\d\d\d/.test(text));
+            return allPdfFiles.map((pdf) => {
+                const [dot, type, year, round] = pdf.path.split("/");
+                const id = `${type}-${year}-${round}`;
+                const folder = [dot, type, year, round].join("/");
+                const inputs = allInputFiles.filter((file) => file.path.startsWith(folder));
+                const document = Document.openDocument("src/content/" + pdf.path);
+                const page = JSON.parse(
+                    document.loadPage(0).toStructuredText().asJSON(),
+                );
+                const lines = page.blocks
+                    .flatMap((block) => block.lines)
+                    .map((line) => line.text.trim());
+                const [title] = lines
+                    .filter((text) => text.length > 3) // Filter things like "EN"
+                    .filter((text) => !/\d\d\d\d/.test(text));
 
-				return {
-					id: `${type}-${year}-${round}`,
-					type,
-					year,
-					round,
-					title,
-					cover: "",
-					pdf: file,
-					inputs,
-				};
-			});
-		},
-		schema: ({ image }) =>
-			z.object({
-				type: z.string().default("hashcodes"),
-				year: z.string(),
-				round: z.string(),
-				title: z.string(),
-				// description: z.string(),
-				cover: image(),
-				pdf: z.string(),
-				inputs: z.array(z.string()),
-			}),
-	}),
+                return {id, type, year, round, title, pdf, inputs};
+            });
+        },
+        schema: ({image}) =>
+            z.object({
+                type: z.string().default("hashcodes"),
+                year: z.string(),
+                round: z.string(),
+                title: z.string(),
+                // description: z.string(),
+                // cover: image(),
+                pdf: z.object({name: z.string(), url: z.string()}),
+                inputs: z.object({name: z.string(), url: z.string()}).array(),
+            }),
+    }),
 };
